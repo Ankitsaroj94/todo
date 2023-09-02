f1bc0d42caa452351c8b33afdddb3f4ffc80fbb8
Ankitsaroj94 <ankitsaroj.ankit94555@gmail.com>
2023-09-02 16:25:24 +0530

    succesfully connected firebase

--- lib/Auth/authform.dart
+++ lib/Auth/authform.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class AuthForm extends StatefulWidget {
-  const AuthForm({super.key});
+  const AuthForm({Key? key}) : super(key: key);
+
 
   @override
   State<AuthForm> createState() => _AuthFormState();
@@ -19,7 +23,45 @@ class _AuthFormState extends State<AuthForm> {
   bool isLoginPage=false;
 
   //-----------------------------------------------
-  @override
+
+  startauthentication()  {
+   bool validity = _formkey.currentState?.validate() ??false;
+    FocusScope.of(context).unfocus();
+
+    if (validity) {
+      _formkey.currentState?.save();
+      submitform(_email,_password,_username);
+    }
+  }
+
+  submitform(String email, String password, String username) async {
+    final auth = FirebaseAuth.instance;
+    UserCredential authResult;
+    try {
+      if (isLoginPage) {
+        authResult = await auth.signInWithEmailAndPassword(email: email, password: password);
+      } else {
+        authResult = await auth.createUserWithEmailAndPassword(email: email, password: password);
+        String? uid = authResult.user?.uid;
+
+        if (uid != null) { // Check if uid is not null
+          await FirebaseFirestore.instance.collection('users').doc(uid).set({
+            'username': username,
+            'email': email,
+          });
+        } else {
+          // Handle the case when uid is null (this should ideally never happen if createUserWithEmailAndPassword was successful)
+          print("Error: UID is null");
+          // You can also show a message to the user or log this incident for debugging.
+        }
+      }
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  //-----------------------------------------------
+   @override
   Widget build(BuildContext context) {
     return Container(
       //------- to set height and width----
@@ -40,14 +82,15 @@ class _AuthFormState extends State<AuthForm> {
                   children: [
                     //-----------username------
 
-                    if(!isLoginPage)
+
                       SizedBox(height: 10),
+                    if(!isLoginPage)
                     TextFormField(
                       keyboardType: TextInputType.emailAddress,
                       key: ValueKey('username'),
                       validator: (value){
-                        if(value!.isNotEmpty){
-                          return 'Incorrect Username';
+                        if(value!.isEmpty) {
+                          return 'Please enter a username';
                         }
                         return null;
                       },
@@ -71,7 +114,7 @@ class _AuthFormState extends State<AuthForm> {
                       keyboardType: TextInputType.emailAddress,
                       key: ValueKey('email'),
                       validator: (value){
-                        if(value!.isEmpty || !value.contains('@') ){
+                        if(value!.isEmpty || !value.contains('@')) {
                           return 'Incorrect Email';
                         }
                         return null;
@@ -94,11 +137,12 @@ class _AuthFormState extends State<AuthForm> {
                   //--------password-----
                     SizedBox(height: 10),
                     TextFormField(
+                      obscureText: true,
                       keyboardType: TextInputType.emailAddress,
                       key: ValueKey('password'),
                       validator: (value){
-                        if(value!.isNotEmpty){
-                          return 'Incorrect Password';
+                        if(value!.isEmpty || value.length < 4) {
+                          return 'Password must be at least 4 characters long';
                         }
                         return null;
                       },
@@ -123,7 +167,9 @@ class _AuthFormState extends State<AuthForm> {
                       width: 150,
                        child: ElevatedButton(
 
-                         onPressed: () { },
+                         onPressed: () {
+                           startauthentication();
+                         },
 
                          style: ElevatedButton.styleFrom(
                            textStyle: TextStyle(fontSize: 25),
@@ -143,33 +189,16 @@ class _AuthFormState extends State<AuthForm> {
                     ),
 
                     SizedBox(height: 10),
-                 /* SizedBox(
-                      height: 60,
-                      width: 150,
-                      child: ElevatedButton(onPressed: () { },
-
-                        style: ElevatedButton.styleFrom(
-                          textStyle: TextStyle(fontSize: 25),
-
-                          backgroundColor: Colors.blue,
-                          minimumSize: Size(88, 36),
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-
-                        ),
-                        child: isLoginPage? Text('Login')
-                            :Text('Sign Up'),
-                      ),
-                    ) */
-
                     TextButton(
                       style: TextButton.styleFrom(
                         textStyle: const TextStyle(fontSize: 20),
                       ),
-                      onPressed: () {},
-                      child: const Text('Enabled'),
+                      onPressed: () {
+                        setState(() {
+                          isLoginPage=!isLoginPage;
+                        });
+                      },
+                      child: isLoginPage?Text('Not a member?'): Text('Already a member?'),
                     ),
                   ],
                 ),
